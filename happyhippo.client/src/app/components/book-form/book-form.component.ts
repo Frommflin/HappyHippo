0d2f083712fe26450c23307f93cd99ca21fdc602
Frommflin <freakyfrommelin@outlook.com>
2024-01-19 17:04:50 +0100

    Adding form-page to add and edit books

---
+++ happyhippo.client/src/app/components/book-form/book-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hippo-book-form',
+  templateUrl: './book-form.component.html',
+  styleUrls: ['../../styles/css/book-form.component.css']
+})
+export class BookFormComponent {
+
+}
